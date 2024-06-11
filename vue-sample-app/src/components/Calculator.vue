5047755cb9ceefba01180b0548a20d7a3d9c6ddb
swathi_v7 <swathinarmgowda@gmail.com>
2024-06-11 16:52:04 +0530

    Logic  changes for operators

--- vue-sample-app/src/components/Calculator.vue
+++ vue-sample-app/src/components/Calculator.vue
@@ -1,93 +1,128 @@
 <template>
   <div class="calculator">
-    <div class = display>{{ current || '0'}}</div>
-    <div @click ="clear" class = "button">C</div>
-    <div @click ="sign" class = "button">+/-</div>
-    <div @click ="percent" class = "button">%</div>
-    <div class = "button operator-confings">/</div>
-    <div @click ="append('7')"class = "button">7</div>
-    <div  @click ="append('8')" class = "button">8</div>
-    <div  @click ="append('9')"class = "button">9</div>
-    <div class = "button operator-confings">x</div>
-    <div  @click ="append('4')"class = "button">4</div>
-    <div  @click ="append('5')"class = "button">5</div>
-    <div  @click ="append('6')" class = "button">6</div>
-    <div class = "button operator-confings">-</div>
-    <div  @click ="append('1')"class = "button">1</div>
-    <div  @click ="append('2')"class = "button">2</div>
-    <div  @click ="append('3')"class = "button">3</div>
-    <div class = "button operator-confings">+</div>
-    <div @click ="append('0')" class = "button zero">0</div>
-    <div @click ="dot" class = "button">.</div>
-    <div class = "button operator-confings">=</div>
-   </div>
+    <div class="display">{{current || '0'}}</div>
+    <div @click="clear" class="btn">C</div>
+    <div @click="sign" class="btn">+/-</div>
+    <div @click="percent" class="btn">%</div>
+    <div @click="setOperator('divide')" class="btn operator">÷</div>
+    <div @click="append('7')" class="btn">7</div>
+    <div @click="append('8')" class="btn">8</div>
+    <div @click="append('9')" class="btn">9</div>
+    <div @click="setOperator('times')" class="btn operator">x</div>
+    <div @click="append('4')" class="btn">4</div>
+    <div @click="append('5')" class="btn">5</div>
+    <div @click="append('6')" class="btn">6</div>
+    <div @click="setOperator('minus')" class="btn operator">-</div>
+    <div @click="append('1')" class="btn">1</div>
+    <div @click="append('2')" class="btn">2</div>
+    <div @click="append('3')" class="btn">3</div>
+    <div @click="setOperator('add')" class="btn operator">+</div>
+    <div @click="append('0')" class="btn zero">0</div>
+    <div @click="dot" class="btn">.</div>
+    <div @click="equal" class="btn operator">=</div>
+  </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        current: '1234'
-      };
+export default {
+  data() {
+    return {
+      previous: null,
+      current: '',
+      operator: null,
+      operatorClicked: false,
+    }
+  },
+  methods: {
+    clear() {
+      this.current = '';
+      this.previous = null;
+      this.operator = null;
     },
-    methods: {
-      clear() {
-        this.current = '';
-      },
-      sign() {
-      this.current = this.current.charAt(0) === '-' ?
-      this.current.slice(1) : `-${this.current}`;
+    sign() {
+      this.current = this.current.charAt(0) === '-' ? 
+        this.current.slice(1) : `-${this.current}`;
     },
-    percent(){
+    percent() {
       this.current = `${parseFloat(this.current) / 100}`;
     },
-    append(number){
+    append(number) {
+      if (this.operatorClicked) {
+        this.current = '';
+        this.operatorClicked = false;
+      }
       this.current = `${this.current}${number}`;
-  },
-  dot(){
-    if(this.current.indexOf('.') === '-1'){
-      this.append('.');
+    },
+    dot() {
+      if (this.current.indexOf('.') === -1) {
+        this.append('.');
+      }
+    },
+    setOperator(op) {
+      if (this.previous !== null) {
+        this.equal();
+      }
+      this.operator = op;
+      this.previous = this.current;
+      this.operatorClicked = true;
+    },
+    equal() {
+      let result = 0;
+      const prev = parseFloat(this.previous);
+      const curr = parseFloat(this.current);
+      if (isNaN(prev) || isNaN(curr)) return;
+
+      switch (this.operator) {
+        case 'add':
+          result = prev + curr;
+          break;
+        case 'minus':
+          result = prev - curr;
+          break;
+        case 'times':
+          result = prev * curr;
+          break;
+        case 'divide':
+          result = prev / curr;
+          break;
+        default:
+          return;
+      }
+
+      this.current = `${result}`;
+      this.previous = null;
+      this.operator = null;
     }
   }
-}    
-};
+}
 </script>
 
-
 <style scoped>
-
-.calculator{
-
+.calculator {
   margin: 0 auto;
   width: 400px;
+  font-size: 40px;
   display: grid;
-
-  /*for defining columns */
-  grid-template-columns: repeat(4, 1fr) ;
-
-  /*for defining rows */
+  grid-template-columns: repeat(4, 1fr);
   grid-auto-rows: minmax(50px, auto);
-
 }
 
-.display{
-  grid-column: 1/5 ;
-  background-color: #eee;
-  color: black;
-  font-weight: bold;
-
+.display {
+  grid-column: 1 / 5;
+  background-color: #333;
+  color: white;
 }
 
-.zero{
-  grid-column: 1/3;
+.zero {
+  grid-column: 1 / 3;
 }
 
-.button{
-  background-color: #eee;
-  border: 1px solid #333;
+.btn {
+  background-color: #F2F2F2;
+  border: 1px solid #999;
 }
 
-.operator-confings{
+.operator {
   background-color: orange;
   color: white;
 }
